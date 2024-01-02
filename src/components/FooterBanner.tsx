af476bed2d2eb2a4081895cff9c0520abc52e72d
Aodhgan Gleeson <aodhgan.gleeson@windranger.io>
2024-01-02 14:19:06 -0800

    use Link

--- src/components/FooterBanner.tsx
+++ src/components/FooterBanner.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Link from 'next/link'
 
 const FooterBanner = () => {
     return (
         <div className="footer-banner">
-            <a href="/faq" className="footer-link">FAQ</a>
+            {/* <a href="/faq" className="footer-link">FAQ</a>
+             */}
+            <Link href="/faq" className="footer-link">FAQ</Link>
             <span className="footer-text">Made with ❤️ by <a href="https://www.mantle.xyz/">Mantle Research Team</a></span>
         </div>
     );
